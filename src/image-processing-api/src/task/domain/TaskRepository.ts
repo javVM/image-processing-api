e5a6d78a782c4f011f823eb69dd1070f920d8bc2
javVM <jverdemarin@gmail.com>
2025-07-04 16:26:21 +0200

    feat(domain): Create TaskRepository

---
+++ src/image-processing-api/src/task/domain/TaskRepository.ts
@@ -0,0 +1,7 @@
+import { ImagePath } from "./ImagePath";
+import { Task } from "./Task";
+
+export interface TaskRepository {
+  createTask(path: ImagePath): Promise<Task>;
+  getTask(taskId: string): Promise<Task | null>;
+}
