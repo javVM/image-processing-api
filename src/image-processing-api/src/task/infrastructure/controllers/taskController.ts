1d8addfdf6938bef9159ab21ef40ab4322a53793
javVM <jverdemarin@gmail.com>
2025-07-05 04:34:11 +0200

    feat(infrastructure): Create controller to run the different use cases

---
+++ src/image-processing-api/src/task/infrastructure/controllers/taskController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { ServiceContainer } from "../services/serviceContainer";
+
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const originalPath = req.body.path;
+    const task = await ServiceContainer.createTask.execute(originalPath);
+    const structuredTask = task.getValue();
+    res.status(201).json(structuredTask);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    }
+  }
+};
+
+export const getTask = async (req: Request, res: Response) => {
+  try {
+    const taskId = req.params.taskId;
+    const task = await ServiceContainer.getTask.execute(taskId);
+    if (!task) {
+      res.status(404).send(`Task with id ${taskId} not found`);
+    }
+    res.status(200).json(task.getValue());
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    }
+  }
+};
