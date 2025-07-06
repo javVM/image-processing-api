30648469a7f07588d5ab04bd0a9db51aaf61374e
javVM <jverdemarin@gmail.com>
2025-07-06 15:47:34 +0200

    refactor(infrastructure): Update image generation method to use custom error handling

--- src/image-processing-api/src/task/infrastructure/imageGenerator/SharpImageGenerator.ts
+++ src/image-processing-api/src/task/infrastructure/imageGenerator/SharpImageGenerator.ts
@@ -1,4 +1,4 @@
-import sharp, { AvailableFormatInfo, FormatEnum } from "sharp";
+import sharp from "sharp";
 import axios from "axios";
 import crypto from "crypto";
 import * as path from "path";
@@ -6,6 +6,7 @@ import * as fs from "fs";
 import { ImageGenerator } from "../../domain/entities/ImageGenerator";
 import { ImagePath } from "../../domain/entities/ImagePath";
 import { Image } from "../../domain/entities/Image";
+import { ImageGenerationError } from "../errors/ImageGenerationError";
 
 export class SharpImageGenerator implements ImageGenerator {
   private availableWidths = [1024, 800];
@@ -22,33 +23,40 @@ export class SharpImageGenerator implements ImageGenerator {
   }
 
   async generate(originalPath: ImagePath): Promise<Image[]> {
-    const images: Image[] = [];
-    const baseDir = process.cwd();
-    const posixBaseDir = baseDir.replace(/\\/g, "/");
-    const outputDir = process.env.OUTPUT_DIR ?? "/output";
-    const originalPathValue = originalPath.value;
-    const fileNameWithExtension = path.basename(originalPath.value);
-    const extension = path.extname(fileNameWithExtension).slice(1);
-    const fileName = path.basename(fileNameWithExtension, `.${extension}`);
-    const imageBuffer = this.isURL(originalPathValue)
-      ? await this.downloadImage(originalPathValue)
-      : fs.readFileSync(path.join(baseDir, originalPathValue));
+    try {
+      const images: Image[] = [];
+      const baseDir = process.cwd();
+      const posixBaseDir = baseDir.replace(/\\/g, "/");
+      const outputDir = process.env.OUTPUT_DIR ?? "/output";
+      const originalPathValue = originalPath.value;
+      const fileNameWithExtension = path.basename(originalPath.value);
+      const extension = path.extname(fileNameWithExtension).slice(1);
+      const fileName = path.basename(fileNameWithExtension, `.${extension}`);
+      const imageBuffer = this.isURL(originalPathValue)
+        ? await this.downloadImage(originalPathValue)
+        : fs.readFileSync(path.join(baseDir, originalPathValue));
 
-    for (const width of this.availableWidths) {
-      const md5Identifier = crypto.createHash("md5").update(width.toString()).digest("hex");
-      const newImageFileName = `${md5Identifier}.${extension}`;
-      const imageOutPutDir = path.posix.join(outputDir, fileName, width.toString());
-      const fullOutputDir = path.posix.join(posixBaseDir, imageOutPutDir);
-      fs.mkdirSync(fullOutputDir, { recursive: true });
-      const outputPath = path.posix.join(fullOutputDir, newImageFileName);
-      const newImagePath = path.posix.join(imageOutPutDir, newImageFileName);
-      await sharp(imageBuffer)
-        .resize({ width })
-        .toFormat(extension as keyof sharp.FormatEnum)
-        .toFile(outputPath);
-      images.push(new Image(new ImagePath(newImagePath), width.toString()));
+      for (const width of this.availableWidths) {
+        const md5Identifier = crypto.createHash("md5").update(width.toString()).digest("hex");
+        const newImageFileName = `${md5Identifier}.${extension}`;
+        const imageOutPutDir = path.posix.join(outputDir, fileName, width.toString());
+        const fullOutputDir = path.posix.join(posixBaseDir, imageOutPutDir);
+        fs.mkdirSync(fullOutputDir, { recursive: true });
+        const outputPath = path.posix.join(fullOutputDir, newImageFileName);
+        const newImagePath = path.posix.join(imageOutPutDir, newImageFileName);
+        await sharp(imageBuffer)
+          .resize({ width })
+          .toFormat(extension as keyof sharp.FormatEnum)
+          .toFile(outputPath);
+        images.push(new Image(new ImagePath(newImagePath), width.toString()));
+      }
+
+      return images;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new ImageGenerationError(error.message);
+      }
+      throw new ImageGenerationError("Unknown error during image generation");
     }
-
-    return images;
   }
 }
