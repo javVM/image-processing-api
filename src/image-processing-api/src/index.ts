9c1e0a083261114145b5c478c82f0ff93e8c7287
javVM <jverdemarin@gmail.com>
2025-07-06 03:23:07 +0200

    feat(config): Add dotenv to support loading environment variables in index.ts

--- src/image-processing-api/src/index.ts
+++ src/image-processing-api/src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import { TaskRouter } from "./task/infrastructure/routes/taskRouter";
 import { connectToDatabase } from "./task/infrastructure/database/mongo/mongoConnection";
 
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "local"}` });
 const app = express();
 app.use(express.json());
 app.use("/tasks", TaskRouter);
 
 async function bootstrap() {
   await connectToDatabase();
-  app.listen(3000, () => {
-    console.log("Image processing API is running on port 3000");
+  app.listen(process.env.PORT, () => {
+    console.log(`Image processing API is running on port ${process.env.PORT}`);
   });
 }
 
