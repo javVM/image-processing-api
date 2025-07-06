6f4914cdc0b2b61813e8035659133330ad3869e1
javVM <jverdemarin@gmail.com>
2025-07-06 17:10:22 +0200

    refactor(domain): Use random price generator inside constructor

--- src/image-processing-api/src/task/domain/entities/Price.ts
+++ src/image-processing-api/src/task/domain/entities/Price.ts
@@ -1,23 +1,26 @@
 import { InvalidPriceError } from "../errors/InvalidPriceError";
 
 export class Price {
-  private static minimumValue = 5;
-  private static maximumValue = 50;
+  private readonly minimumValue = 5;
+  private readonly maximumValue = 50;
   value: number;
 
-  constructor(value: number) {
-    Price.checkIsValid(value);
-    this.value = value;
+  constructor(value?: number) {
+    if (value) {
+      this.checkIsValid(value);
+      this.value = value;
+    } else {
+      this.value = this.getRandomPrice();
+    }
   }
 
-  private static checkIsValid(value: number) {
+  private checkIsValid(value: number) {
     if (value < this.minimumValue || value > this.maximumValue) {
       throw new InvalidPriceError(this.minimumValue, this.maximumValue);
     }
   }
 
-  static random(): Price {
-    const randomPrice = Math.floor(Math.random() * (this.maximumValue - this.minimumValue + 1)) + this.minimumValue;
-    return new Price(randomPrice);
+  private getRandomPrice(): number {
+    return Math.floor(Math.random() * (this.maximumValue - this.minimumValue + 1)) + this.minimumValue;
   }
 }
