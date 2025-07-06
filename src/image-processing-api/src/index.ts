34dfa8fba2af8c73cc9019564e27871b66bfc30c
javVM <jverdemarin@gmail.com>
2025-07-06 16:37:31 +0200

    feat(infrastructure): Create error handler middleware to centralize error management

--- src/image-processing-api/src/index.ts
+++ src/image-processing-api/src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import { TaskRouter } from "./task/infrastructure/routes/taskRouter";
 import { connectToDatabase } from "./task/infrastructure/database/mongo/mongoConnection";
+import { errorHandler } from "./task/infrastructure/middlewares/errorHandler";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV || "local"}` });
 const app = express();
 app.use(express.json());
 app.use("/tasks", TaskRouter);
+app.use(errorHandler);
 
 async function bootstrap() {
   await connectToDatabase();
