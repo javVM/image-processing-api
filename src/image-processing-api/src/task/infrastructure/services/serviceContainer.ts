0d12a0f104caae95145f20bbe54332d41c867664
javVM <jverdemarin@gmail.com>
2025-07-06 16:36:33 +0200

    feat(infrastructure): Add UpdateTaskStatus use case to service container

--- src/image-processing-api/src/task/infrastructure/services/serviceContainer.ts
+++ src/image-processing-api/src/task/infrastructure/services/serviceContainer.ts
@@ -1,6 +1,7 @@
 import { CreateTask } from "../../application/createTask/CreateTask";
 import { GenerateTaskImages } from "../../application/generateTaskImages/GenerateTaskImages";
 import { GetTask } from "../../application/getTask/GetTask";
+import { UpdateTaskStatus } from "../../application/updateTaskStatus/UpdateTaskStatus";
 import { SharpImageGenerator } from "../imageGenerator/SharpImageGenerator";
 import { MongoImageRepository } from "../repositories/MongoImageRepository";
 import { MongoTaskRepository } from "../repositories/MongoTaskRepository";
@@ -12,5 +13,6 @@ const imageGenerator = new SharpImageGenerator();
 export const ServiceContainer = {
   getTask: new GetTask(taskRepository),
   createTask: new CreateTask(taskRepository),
+  updateTaskStatus: new UpdateTaskStatus(taskRepository),
   generateTaskImages: new GenerateTaskImages(imageGenerator, imageRepository),
 };
