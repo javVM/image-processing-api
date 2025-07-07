1fbf4cad924720b963cd6e146743dc0ce80b6874
javVM <jverdemarin@gmail.com>
2025-07-07 13:20:16 +0200

    refactor(populate): Divide code into simpler methods

--- src/image-processing-api/populate/populate.ts
+++ src/image-processing-api/populate/populate.ts
@@ -1,23 +1,32 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { TaskModel } from "../src/task/infrastructure/database/mongo/TaskModel";
-import { ImageModel } from "../src/task/infrastructure/database/mongo/ImageModel";
+import { TaskDocument, TaskModel } from "../src/task/infrastructure/database/mongo/TaskModel";
+import { ImageDocument, ImageModel } from "../src/task/infrastructure/database/mongo/ImageModel";
 import path from "path";
+
+export function loadDataFromFiles() {
+  const images = require(path.join(process.cwd(), "/data/images.json"));
+  const tasks = require(path.join(process.cwd(), "/data/tasks.json"));
+  return { images, tasks };
+}
+
+export async function connectToDatabase() {
+  dotenv.config({ path: `.env.local` });
+  const MONGODB_URI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.DATABASE}`;
+  await mongoose.connect(MONGODB_URI);
+  console.log("Connected to database");
+}
+
+export async function populateDatabase(images: Object, tasks: Object) {
+  await Promise.all([ImageModel.deleteMany({}), TaskModel.deleteMany({})]);
+  await Promise.all([ImageModel.insertMany(images), TaskModel.insertMany(tasks)]);
+}
 
 async function populate() {
   try {
-    const images = require(path.join(process.cwd(), "/data/images.json"));
-    const tasks = require(path.join(process.cwd(), "/data/tasks.json"));
-
-    dotenv.config({ path: `.env.local` });
-
-    const MONGODB_URI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.DATABASE}`;
-    await mongoose.connect(MONGODB_URI);
-    console.log("Connected to MongoDB");
-
-    await Promise.all([ImageModel.deleteMany({}), TaskModel.deleteMany({})]);
-    await Promise.all([ImageModel.insertMany(images), TaskModel.insertMany(tasks)]);
-
+    const loadedData = loadDataFromFiles();
+    await connectToDatabase();
+    await populateDatabase(loadedData.images, loadedData.tasks);
     console.log("Database populated!");
     process.exit(0);
   } catch (err) {
